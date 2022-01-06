72a3388c042f4812e2db33f6d6a1b757392a18a6
jacopograndi <jacopo.grandi@outlook.it>
2022-01-06 18:33:02 +0100

    day08

--- utils.h
+++ utils.h
@@ -9,4 +9,18 @@ void split (std::vector<std::string> &vec, std::string str, std::string del) {
     } else { vec.push_back(str); }
 }
 
+// trim
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+static inline void trim(std::string &s) { ltrim(s); rtrim(s); }
+
+
 #endif
